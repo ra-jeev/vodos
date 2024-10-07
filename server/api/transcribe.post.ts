cb5798f8f462dd869b3699bd9c40e625af552a98
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-07 23:04:10 +0530

    feat: add audio transcription for incoming audio

--- server/api/transcribe.post.ts
+++ server/api/transcribe.post.ts
@@ -1,18 +1,31 @@
 export default defineEventHandler(async (event) => {
   console.log('incoming transcribe request', event.headers);
 
-  const files = await readMultipartFormData(event);
+  const formData = await readMultipartFormData(event);
   console.log(
     'incoming transcribe request with multipart length: ',
-    files?.length
+    formData?.length
   );
 
-  if (files?.length) {
-    const formData = files[0];
-    console.log(formData.name, formData.filename, formData.data.byteLength);
+  if (formData?.length) {
+    const fileData = formData[0];
+    console.log(fileData.name, fileData.filename, fileData.data.byteLength);
+
+    const input = {
+      audio: [...new Uint8Array(fileData.data)],
+    };
+
+    const response = await hubAI().run('@cf/openai/whisper-tiny-en', input);
+    // const response = await hubAI().run('@cf/openai/whisper', input);
+
+    return {
+      success: true,
+      response,
+    };
   }
 
   return {
-    success: true,
+    success: false,
+    response: null,
   };
 });
