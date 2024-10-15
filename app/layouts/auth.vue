5b3df684ba68871182722ee0194485918ac4050f
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-15 23:03:23 +0530

    feat: add auth and default layouts

---
+++ app/layouts/auth.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="flex flex-col min-h-screen items-center justify-center gap-y-10">
+    <AppLogo text-size="text-2xl md:text-3xl" icon-size="w-10 h-10" />
+
+    <slot />
+  </main>
+</template>
+
+<script setup lang="ts">
+const { loggedIn } = useUserSession();
+
+watchEffect(() => {
+  if (loggedIn.value) {
+    navigateTo('/');
+  }
+});
+</script>
