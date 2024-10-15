7eaa3a510abcc77b1d7fcd3fd981ae36349a69ce
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-15 23:06:27 +0530

    chore: minor changes

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,20 @@
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <UNotifications />
 </template>
+
+<script setup lang="ts">
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  bodyAttrs: {
+    class: 'bg-white dark:bg-gray-900',
+  },
+});
+</script>
