79fb30b335f8c506b95b8bc1e7477b0701c93e21
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-17 23:02:24 +0530

    chore: minor default layout change

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="min-h-screen">
+  <div class="flex flex-col min-h-screen">
     <AppHeader />
 
-    <main>
+    <main class="flex-1">
       <slot />
     </main>
   </div>
