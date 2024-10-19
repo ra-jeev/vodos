c94319090246d9b6e107c133084dfad646f9a94e
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-19 23:07:38 +0530

    feat: Add unified ToDoInput for voice & manual mode

--- app/components/ToDoInput.vue
+++ app/components/ToDoInput.vue
@@ -1,44 +1,36 @@
 <template>
-  <form class="flex flex-wrap w-full gap-4" @submit.prevent="handleSubmit">
-    <UInput
-      v-model="newTodo"
-      placeholder="Add a new todo..."
-      autofocus
-      class="flex-1"
-    />
-
-    <div class="flex w-full md:w-auto gap-x-4">
-      <UInput v-model="todoDueDate" type="date" class="flex-1" />
-
-      <UButton
-        type="submit"
-        icon="i-heroicons-plus-16-solid"
-        :disabled="!newTodo"
-        :loading="loading"
-        aria-label="Add ToDo"
+  <UCard
+    :ui="{
+      rounded: 'rounded-lg',
+      shadow: 'shadow-lg',
+      body: {
+        base: ' relative',
+        padding: 'pt-8 sm:pt-8',
+      },
+    }"
+  >
+    <div
+      :class="[
+        'inline-flex items-center h-8 gap-x-3 rounded-full absolute left-1/2 -top-4 -translate-x-1/2 border px-2.5 cursor-pointer bg-white dark:bg-gray-900',
+        isVoiceMode ? 'border-primary' : 'border-gray-500',
+      ]"
+      @click="isVoiceMode = !isVoiceMode"
+    >
+      <UToggle :model-value="isVoiceMode" />
+      <span
+        class="text-xs shrink-0"
+        :class="isVoiceMode ? 'text-primary' : 'text-gray-500'"
       >
-        Add
-      </UButton>
+        {{ isVoiceMode ? 'Voice Mode' : 'Manual Mode' }}
+      </span>
     </div>
-  </form>
+
+    <VoiceInput v-if="isVoiceMode" />
+
+    <ManualInput v-else />
+  </UCard>
 </template>
 
 <script setup lang="ts">
-const emit = defineEmits(['add-todo']);
-
-const newTodo = ref('');
-const todoDueDate = ref('');
-const loading = ref(false);
-
-const handleSubmit = () => {
-  if (!newTodo.value.trim()) return;
-
-  emit('add-todo', {
-    text: newTodo.value,
-    todoAt: todoDueDate.value,
-  });
-
-  newTodo.value = '';
-  todoDueDate.value = '';
-};
+const isVoiceMode = ref(false);
 </script>
