9016c3596c5db5a80f66bb78452d1625fcf27a4c
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-20 23:07:11 +0530

    chore: update ai utils to call addToDo function

--- server/utils/ai.ts
+++ server/utils/ai.ts
@@ -1,18 +1,42 @@
 import { runWithTools } from '@cloudflare/ai-utils';
 
-export async function handleUserRequest(message: string) {
+const handleCreateToDo = async (
+  userId: string,
+  todo: string,
+  todoAt?: string
+) => {
+  console.log('detected todo:', todo, 'time: ', todoAt);
+  let todoAtDate: Date | undefined = undefined;
+  if (todoAt) {
+    todoAtDate = new Date(todoAt);
+    if (isNaN(todoAtDate.getTime())) {
+      todoAtDate = undefined;
+    }
+  }
+
+  try {
+    await addToDo(userId, todo, todoAtDate);
+
+    return JSON.stringify({ todo, todoAt });
+  } catch (error) {
+    console.error('Error adding todo:', error);
+  }
+
+  return ''
+};
+
+export async function handleUserMessage(userId: string, message: string) {
   const messages: { role: 'user' | 'system'; content: string }[] = [
+    {
+      role: 'system',
+      content:
+        "You are a helpful personal assistant who manages the Todo list of users. You can use the available tools to help you perform your duties. For add ToDo tasks you'll infer the ToDo that the user wants to add, and if any ToDo date that can be inferred from the user message.",
+    },
     {
       role: 'user',
       content: message,
     },
   ];
-
-  messages.unshift({
-    role: 'system',
-    content:
-      "You are a helpful personal assistant who manages the Todo list of users. You can use the available tools to help you perform your duties. For add ToDo tasks you'll infer the ToDo that the user wants to add, and if any ToDo date that can be inferred from the user message.",
-  });
 
   const response = await runWithTools(
     hubAI(),
@@ -22,7 +46,7 @@ export async function handleUserRequest(message: string) {
       tools: [
         {
           name: 'createTodo',
-          description: 'Creates a new todo/task',
+          description: 'Creates a new todo',
           parameters: {
             type: 'object',
             properties: {
@@ -33,15 +57,13 @@ export async function handleUserRequest(message: string) {
               todoAt: {
                 type: 'string',
                 description:
-                  '(Optional) The date to which the user wants to add this ToDo to. Format: YYYY-MM-DD',
+                  '(Optional) The ude date of the ToDo. Format: YYYY-MM-DD',
               },
             },
             required: ['todo'],
           },
-          function: async ({ todo, todoAt }) => {
-            console.log('detected todo:', todo, 'time: ', todoAt);
-            return JSON.stringify({ todo, todoAt });
-          },
+          function: ({ todo, todoAt }) =>
+            handleCreateToDo(userId, todo, todoAt),
         },
       ],
     },
