94267c722a83fb9086b401d31c32c9ad1dace352
Rajeev R Sharma <i.rarsh@gmail.com>
2024-10-21 23:59:21 +0530

    feat: add getToDos server utils function

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,4 +1,4 @@
-export async function addToDo(text: string, todoAt?: Date) {
+export async function addToDo(userId: string, text: string, todoAt?: Date) {
   let query: string;
   let bindValues: string[];
 
@@ -16,4 +16,29 @@ export async function addToDo(text: string, todoAt?: Date) {
     .run();
 
   return { id: response.meta.last_row_id };
+}
+
+export async function getToDos(
+  userId: string,
+  filters?: { dueDate?: Date; completed?: boolean }
+) {
+  let query = 'SELECT * FROM todos WHERE userId = ?1';
+  const params: string[] = [userId];
+
+  if (filters?.dueDate) {
+    query += ' AND todoAt = ?2';
+    params.push(filters.dueDate.toISOString());
+  }
+
+  if (typeof filters?.completed !== 'undefined') {
+    query += ` AND completed = ?${params.length + 1}`;
+    params.push(filters.completed ? 'TRUE' : 'FALSE');
+  }
+
+  const response = await hubDatabase()
+    .prepare(query)
+    .bind(...params)
+    .all();
+
+  return response.results;
 }
